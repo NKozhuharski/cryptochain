92cadc93abc454b14d1280e4aedd3559335b2f38
Nikolay Kozhuharski <nikolay.kozhuharski@softwaregroup.com>
2022-03-14 09:09:43 +0200

    Add client block and transaction components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Blocks from "./Blocks";
+import {Link} from 'react-router-dom'
 import logo from "./assets/logo2.png";
 
 class App extends Component {
@@ -18,12 +18,14 @@ class App extends Component {
       <div className="App">
         <img className="logo" src={logo}></img>
         <div>Welcome ro the blockchain...</div>
+        <br />
+        <div><Link to='/blocks'>Blocks</Link></div>
+        <div><Link to='/conduct-transaction'>Conduct a Transaction</Link></div>
+        <br />
         <div className="WalletInfo">
           <div>Address: {address}</div>
           <div>Balance: {balance}</div>
         </div>
-        <br />
-        <Blocks />
       </div>
     );
   }
--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import Block from "./Block";
 
 class Blocks extends Component {
@@ -17,6 +19,7 @@ class Blocks extends Component {
 
     return (
       <div>
+        <div><Link to='/'>Home</Link></div>
         <h3>Blocks</h3>
         {this.state.blocks.map((block) => {
           return <Block key={block.hash} block={block} />;
