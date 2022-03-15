b275b40734faf14fea5c64372886f67e4929ce79
Nikolay Kozhuharski <nikolay.kozhuharski@softwaregroup.com>
2022-03-15 09:07:57 +0200

    Add TransactionPool and ConductTransaction components

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -63,7 +63,7 @@ class Block extends Component {
     return (
       <div className="Block">
         <div>Hash: {hashDisplay}</div>
-        <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
+        <div>Timestamp: {new Date(timestamp).ocaleString()}</div>
         {this.displayTransaction}
       </div>
     );
